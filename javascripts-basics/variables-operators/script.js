c1fbea564431d9b4b34e3730cc1294309ce0a927
Amir Burokur <aamirbu23@gmail.com>
2026-04-28 13:29:25 +0400

    Completed basic

---
+++ javascripts-basics/variables-operators/script.js
@@ -0,0 +1,16 @@
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName);
+console.log(lastName);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+pi = 10;
+
+console.log(pi);
